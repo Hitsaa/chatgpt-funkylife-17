378de83a40c73a12862a558aa1c572dfc974f1b9
hitendra29 <hitendra.shekhawat@ongraph.com>
2023-03-25 14:25:08 +0530

    added new api key

--- src/main/java/com/chatgpt/funkylife/config/ChatGptConfig.java
+++ src/main/java/com/chatgpt/funkylife/config/ChatGptConfig.java
@@ -6,7 +6,8 @@ public class ChatGptConfig {
     public static final String BEARER = "Bearer ";
     // public static final String API_KEY = "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";
     // public static final String API_KEY = "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";
-    public static final String API_KEY = "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";
+    // public static final String API_KEY = "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";
+    public static final String API_KEY = "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";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
